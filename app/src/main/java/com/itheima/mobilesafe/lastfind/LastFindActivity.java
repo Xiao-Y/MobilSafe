315beab394c1bf13b3595d4f640e1a597981ab17
Xiao-Y <lyongdtao123@126.com>
2016-08-24 21:42:25 +0800

    完成手机防盗页面

--- app/src/main/java/com/itheima/mobilesafe/lastfind/LastFindActivity.java
+++ app/src/main/java/com/itheima/mobilesafe/lastfind/LastFindActivity.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.itheima.mobilesafe.lastfind.pagesub.Setup1Activity;
 import com.itheima.mobilesafe.R;
+import com.itheima.mobilesafe.lastfind.pagesub.Setup1Activity;
 
 /**
  * 手机防盗主页
@@ -18,6 +20,8 @@ import com.itheima.mobilesafe.R;
 public class LastFindActivity extends Activity {
 
     private SharedPreferences ps;
+    private TextView tv_safenumber;
+    private ImageView iv_protecting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,18 @@ public class LastFindActivity extends Activity {
         // 是否做过设置向导
         if (configed) {
             this.setContentView(R.layout.activity_lost_find);
+
+            tv_safenumber = (TextView) findViewById(R.id.tv_safenumber);
+            String phone = ps.getString("phone", "");
+            tv_safenumber.setText(phone);
+
+            iv_protecting = (ImageView) findViewById(R.id.iv_protecting);
+            Boolean protecting = ps.getBoolean("protecting", false);
+            if (protecting) {
+                iv_protecting.setImageResource(R.mipmap.lock);
+            } else {
+                iv_protecting.setImageResource(R.mipmap.unlock);
+            }
         } else {
             reEnterSetup(null);
         }
--- app/src/main/java/com/itheima/mobilesafe/lastfind/pagesub/Setup4Activity.java
+++ app/src/main/java/com/itheima/mobilesafe/lastfind/pagesub/Setup4Activity.java
@@ -1,10 +1,11 @@
 package com.itheima.mobilesafe.lastfind.pagesub;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import com.itheima.mobilesafe.R;
 import com.itheima.mobilesafe.lastfind.LastFindActivity;
@@ -19,13 +20,43 @@ import com.itheima.mobilesafe.utils.ActivityChange;
  */
 public class Setup4Activity extends BaseSetupActivity {
 
-    private SharedPreferences ps;
+    //private SharedPreferences sp;
+
+    private CheckBox cbStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ps = getSharedPreferences("config", MODE_PRIVATE);
+        // sp = getSharedPreferences("config", MODE_PRIVATE);
         this.setContentView(R.layout.activity_setup4);
+        cbStatus = (CheckBox) findViewById(R.id.cb_status);
+        //初始化复选框的状态
+        boolean protecting = sp.getBoolean("protecting", false);
+        if (protecting) {
+            cbStatus.setChecked(true);
+            cbStatus.setText("您已经开启防盗保护");
+        } else {
+            cbStatus.setChecked(false);
+            cbStatus.setText("您还没有开启防盗保护");
+        }
+
+        cbStatus.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean protecting) {
+                if (protecting) {
+                    cbStatus.setChecked(true);
+                    cbStatus.setText("您已经开启防盗保护");
+                } else {
+                    cbStatus.setChecked(false);
+                    cbStatus.setText("您还没有开启防盗保护");
+                }
+
+                //保存选择的状态
+                Editor edit = sp.edit();
+                edit.putBoolean("protecting", protecting);
+                edit.commit();
+            }
+        });
     }
 
     /**
@@ -34,7 +65,7 @@ public class Setup4Activity extends BaseSetupActivity {
      * @param view
      */
     public void next(View view) {
-        Editor edit = ps.edit();
+        Editor edit = sp.edit();
         edit.putBoolean("configed", true);
         edit.commit();
         Intent intent = new Intent(Setup4Activity.this, LastFindActivity.class);
