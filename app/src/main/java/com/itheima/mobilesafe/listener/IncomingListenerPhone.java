aec0625dd11c00212ad4365c27da6200c0520168
Xiao-Y <lyongdtao123@126.com>
2016-10-01 22:05:39 +0800

    通讯卫士：优化黑名单拦截，删除呼叫记录

--- app/src/main/java/com/itheima/mobilesafe/listener/IncomingListenerPhone.java
+++ app/src/main/java/com/itheima/mobilesafe/listener/IncomingListenerPhone.java
@@ -1,8 +1,16 @@
 package com.itheima.mobilesafe.listener;
 
+import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.provider.CallLog;
+import android.support.v4.app.ActivityCompat;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.view.View;
@@ -25,6 +33,7 @@ import java.lang.reflect.Method;
  */
 public class IncomingListenerPhone extends PhoneStateListener {
     public final static String TAG = "IncomingListenerPhone";
+    private Context context = MyApplication.getContext();
 
     /**
      * 监听标志：来电、去电归属地显示
@@ -62,10 +71,13 @@ public class IncomingListenerPhone extends PhoneStateListener {
                     String address = NumberAddressQueryUtils.queryNumber(incomingNumber);
                     ToastUtils.myToast(wm, view, address);
                 } else if (listenerFlag == LISTENER_FLAG_BALCK_NUMBER) {
-                    BlackNumberDao dao = new BlackNumberDao(MyApplication.getContext());
+                    BlackNumberDao dao = new BlackNumberDao(context);
                     try {
                         String mode = dao.findMode(incomingNumber);
                         if ("2".equals(mode) || "3".equals(mode)) {
+                            //注册内容观察器，当呼叫记录改变时，说明已经挂断电话，然后删除呼叫记录
+                            this.callLogContentObserver(incomingNumber);
+                            //这个是远程调用的程序，不知道什么时候执行完，所有在删除记录的时候要使用内容观察器处理
                             this.endCall();
                             LogUtil.i(TAG, "电话被挂断...");
                         }
@@ -85,6 +97,35 @@ public class IncomingListenerPhone extends PhoneStateListener {
             default:
                 break;
         }
+    }
+
+    /**
+     * 删除呼叫记录（从内容提供器中获取）<br/>
+     * endCall()是远程调用的程序，不知道什么时候执行完，所有在删除记录的时候要使用内容观察器处理
+     *
+     * @param incomingNumber 要删除的电话号码
+     */
+    private void callLogContentObserver(final String incomingNumber) {
+        //android5.0后动态校验权限
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.WRITE_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        //获取内容提供器的uri
+        final Uri uri = CallLog.CONTENT_URI;
+        final ContentResolver resolver = context.getContentResolver();
+        // 注册内容观察者
+        resolver.registerContentObserver(uri, true, new ContentObserver(new Handler()) {
+            @Override
+            public void onChange(boolean selfChange) {
+                // 取消内容观察者（只用关注一瞬间）
+                resolver.unregisterContentObserver(this);
+                LogUtil.i(TAG, "产生了呼叫记录....");
+                // 删除呼叫记录
+                resolver.delete(uri, "number = ?", new String[] { incomingNumber });
+                LogUtil.i(TAG, "呼叫记录被删除...");
+                super.onChange(selfChange);
+            }
+        });
     }
 
     /**
